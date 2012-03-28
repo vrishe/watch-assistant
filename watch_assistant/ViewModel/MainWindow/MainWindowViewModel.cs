3d525778d4fb0e830dac2ab4c91e622586bad705
AllierG <AllierG@gmail.com>
2012-03-28 22:09:39 +0000

    TODO: 1. GridResizer style 2. Buttons style 3. Window behaviors 4. Static ListBoxMargin

--- watch_assistant/ViewModel/MainWindow/MainWindowViewModel.cs
+++ watch_assistant/ViewModel/MainWindow/MainWindowViewModel.cs
@@ -3,19 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows;
+using System.Windows.Input;
 
 namespace watch_assistant.ViewModel.MainWindow
 {
     class MainWindowViewModel : WindowViewModel
     {
 
+        #region Properties
+        public static int f;
+        #endregion // Properties
+
         #region Constructors
 
         public MainWindowViewModel(Window owner)
             : base(owner)
         {
+            base._owner.CommandBindings.Add(new CommandBinding(ApplicationCommands.Close, (s, e) => { owner.Close(); }));
         }
 
         #endregion // Constructors
+
     }
 }
