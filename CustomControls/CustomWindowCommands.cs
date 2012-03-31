ef6a8146aee3d32ba5df4b05b78ac3c116f7884e
vlados.v10.0 <vlados.v10.0@gmail.com>
2012-03-31 19:22:38 +0000

    Up!

---
+++ CustomControls/CustomWindowCommands.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace CustomControls
+{
+    public static class CustomWindowCommands
+    {
+        public static readonly RoutedUICommand Maximize = new RoutedUICommand();
+        public static RoutedUICommand Minimize = new RoutedUICommand();
+        public static RoutedUICommand DisplaySystemButtons = new RoutedUICommand();
+    }
+}
